90e7174e72da7b8b7e86b81618a30acd77edb4c4
Unknown <orkich@gmail.com>
2018-06-02 18:58:28 +0300

    Auto stash before merge of "master" and "origin/master"

---
+++ PC_Modernisator3000/PC_Modernisator3000/Analyser.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PC_Modernisator3000
+{
+    public partial class Analyser : Form
+    {
+        string pathFile;
+
+        public Analyser()
+        {
+            InitializeComponent();
+            if(!loadFile())
+            {
+                Load += (s, e) => Close();
+                return;
+            }
+            var analysitor = new Analysis(pathFile);
+            var problemDetails = analysitor.run();
+        }
+
+        private bool loadFile()
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            dialog.Filter = "json files (*.json)|*.json";
+            dialog.FilterIndex = 1;
+            dialog.RestoreDirectory = true;
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                pathFile = dialog.FileName;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ PC_Modernisator3000/PC_Modernisator3000/Analysis.cs
@@ -0,0 +1,106 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PC_Modernisator3000
+{
+    enum Problem { OK, GPU, HDD, RAM, CPU };
+    class Analysis
+    {
+        Dictionary<string, DataClass> log;
+        public Analysis(string path)
+        {
+            // This text is added only once to the file.
+            if (File.Exists(path))
+            {
+                string json = File.ReadAllText(path);
+                log = JsonConvert.DeserializeObject<Dictionary<string, DataClass>>(json);
+            }
+        }
+
+        public Dictionary<Problem, double> run()
+        {
+            var vals = log.Values;
+            var problems = new Dictionary<Problem, double>();
+            foreach(var val in vals)
+            {
+                var res = isProblematic(val);
+                foreach(var problem in res)
+                {
+                    if (problems.ContainsKey(problem))
+                    {
+                        problems[problem] += 1;
+                    }
+                    else
+                    {//init key
+                        problems[problem] = 1;
+                    }
+                }
+                
+            }
+            foreach (var key in problems.Keys.ToList())
+                problems[key] = problems[key] / vals.Count;
+            return problems;
+        }
+
+        private List<Problem> isProblematic(DataClass data)
+        {
+            var listOfProblems = new List<Problem>();
+            //Check load hdd
+            foreach(var hdd in data.hdd)
+            {
+                if(hdd.used_space > 90.0)
+                {
+                    listOfProblems.Add(Problem.HDD);
+                }
+            }
+            //check load ram
+            if(data.memory.used_space > 95.0)
+            {
+                listOfProblems.Add(Problem.RAM);
+            }
+            //check cpu temp
+            if (data.cpu.temp > 70.0)
+            {
+                listOfProblems.Add(Problem.CPU);
+            }
+            //check cpu load
+            if (data.cpu.load > 90.0)
+            {
+                listOfProblems.Add(Problem.CPU);
+            }
+            //check cpu trotling
+            if(data.cpu.temp > 70.0 && data.cpu.load < 50.0)
+            {
+                listOfProblems.Add(Problem.CPU);
+            }
+
+            //check gpu temp
+            if (data.gpu.temp > 90.0)
+            {
+                listOfProblems.Add(Problem.GPU);
+            }
+            //check gpu load
+            if (data.gpu.load > 90.0)
+            {
+                listOfProblems.Add(Problem.GPU);
+            }
+            //check gpu memload
+            if (data.gpu.gpumem_load > 90.0)
+            {
+                listOfProblems.Add(Problem.GPU);
+            }
+            //check gpu trotling
+            if (data.gpu.temp > 90.0 && data.gpu.load < 50.0)
+            {
+                listOfProblems.Add(Problem.GPU);
+            }
+            return listOfProblems;
+        }
+    }
+}
