1b8e459938a8a74f05ecfd99bb2cb37e91886af3
Unknown <orkich@gmail.com>
2018-06-02 22:50:43 +0300

    [add] Added charts and analyses

--- PC_Modernisator3000/PC_Modernisator3000/Analyser.cs
+++ PC_Modernisator3000/PC_Modernisator3000/Analyser.cs
@@ -7,13 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ZedGraph;
 
 namespace PC_Modernisator3000
 {
     public partial class Analyser : Form
     {
         string pathFile;
-
+        GraphPane piePane;
+        GraphPane piePaneProblems;
         public Analyser()
         {
             InitializeComponent();
@@ -23,7 +25,10 @@ namespace PC_Modernisator3000
                 return;
             }
             var analysitor = new Analysis(pathFile);
-            var problemDetails = analysitor.run();
+            analysitor.run();
+            drawParts(analysitor.problemParts);
+            drawProblems(analysitor.indicatedProblems);
+
         }
 
         private bool loadFile()
@@ -43,6 +48,42 @@ namespace PC_Modernisator3000
             {
                 return false;
             }
+        }
+
+        private void drawParts(Dictionary<ProblemParts, double> data)
+        {
+            ChartView.GraphPane.CurveList.Clear();
+            ChartView.GraphPane.GraphObjList.Clear();
+            piePane = ChartView.GraphPane;
+            piePane.Title.Text = "Ratio of dangerous state to normal for parts";
+            var labels = new List<string>();
+            var values = new List<double>();
+            foreach (var key in data.Keys)
+            {
+                values.Add(data[key]);
+                labels.Add(key.ToString());
+            }
+            piePane.AddPieSlices(values.ToArray(), labels.ToArray());
+            ChartView.AxisChange();
+            ChartView.Invalidate();
+        }
+
+        private void drawProblems(Dictionary<Problems, double> data)
+        {
+            pieChartProblems.GraphPane.CurveList.Clear();
+            pieChartProblems.GraphPane.GraphObjList.Clear();
+            piePaneProblems = pieChartProblems.GraphPane;
+            piePaneProblems.Title.Text = "Ratio of finded problems";
+            var labels = new List<string>();
+            var values = new List<double>();
+            foreach (var key in data.Keys)
+            {
+                values.Add(data[key]);
+                labels.Add(key.ToString());
+            }
+            piePaneProblems.AddPieSlices(values.ToArray(), labels.ToArray());
+            pieChartProblems.AxisChange();
+            pieChartProblems.Invalidate();
         }
     }
 }
--- PC_Modernisator3000/PC_Modernisator3000/Analysis.cs
+++ PC_Modernisator3000/PC_Modernisator3000/Analysis.cs
@@ -9,10 +9,17 @@ using System.Windows.Forms;
 
 namespace PC_Modernisator3000
 {
-    enum Problem { OK, GPU, HDD, RAM, CPU };
+    enum ProblemParts { OK, GPU, HDD, RAM, CPU };
+    enum Problems {
+        OK, HDD_LACK_OF_MEMORY, RAM_LACK_OF_MEMORY, CPU_OVERHEAT, CPU_OVERLOAD,
+        CPU_TROTTING, GPU_OVERHEAT, GPU_OVERLOAD, GPU_LACK_OF_MEMORY, GPU_TROTTING
+    };
     class Analysis
     {
         Dictionary<string, DataClass> log;
+        public Dictionary<ProblemParts, double> problemParts = new Dictionary<ProblemParts, double>();
+        public Dictionary<Problems, double> indicatedProblems = new Dictionary<Problems, double>();
+
         public Analysis(string path)
         {
             // This text is added only once to the file.
@@ -23,82 +30,160 @@ namespace PC_Modernisator3000
             }
         }
 
-        public Dictionary<Problem, double> run()
+        public void run()
+        {
+            analyse();
+        }
+        private void analyse()
         {
             var vals = log.Values;
-            var problems = new Dictionary<Problem, double>();
-            foreach(var val in vals)
+            foreach (var val in vals)
             {
-                var res = isProblematic(val);
-                foreach(var problem in res)
+                var resParts = analyzeParts(val);
+                var resProblems = analyzeProblems(val);
+                foreach (var part in resParts)
                 {
-                    if (problems.ContainsKey(problem))
+                    if (problemParts.ContainsKey(part))
                     {
-                        problems[problem] += 1;
+                        problemParts[part] += 1;
                     }
                     else
                     {//init key
-                        problems[problem] = 1;
+                        problemParts[part] = 1;
                     }
                 }
-                
+
+                foreach (var problem in resProblems)
+                {
+                    if (indicatedProblems.ContainsKey(problem))
+                    {
+                        indicatedProblems[problem] += 1;
+                    }
+                    else
+                    {//init key
+                        indicatedProblems[problem] = 1;
+                    }
+                }
+
             }
-            foreach (var key in problems.Keys.ToList())
-                problems[key] = problems[key] / vals.Count;
-            return problems;
+            foreach (var key in problemParts.Keys.ToList())
+                problemParts[key] = problemParts[key] / problemParts.Keys.Count;
+
+            foreach (var key in indicatedProblems.Keys.ToList())
+                indicatedProblems[key] = indicatedProblems[key] / indicatedProblems.Keys.Count;
+            return;
         }
 
-        private List<Problem> isProblematic(DataClass data)
+        private List<ProblemParts> analyzeParts(DataClass data)
         {
-            var listOfProblems = new List<Problem>();
+            var listOfProblems = new List<ProblemParts>();
             //Check load hdd
             foreach(var hdd in data.hdd)
             {
                 if(hdd.used_space > 90.0)
                 {
-                    listOfProblems.Add(Problem.HDD);
+                    listOfProblems.Add(ProblemParts.HDD);
                 }
             }
             //check load ram
             if(data.memory.used_space > 95.0)
             {
-                listOfProblems.Add(Problem.RAM);
+                listOfProblems.Add(ProblemParts.RAM);
             }
             //check cpu temp
             if (data.cpu.temp > 70.0)
             {
-                listOfProblems.Add(Problem.CPU);
+                listOfProblems.Add(ProblemParts.CPU);
             }
             //check cpu load
             if (data.cpu.load > 90.0)
             {
-                listOfProblems.Add(Problem.CPU);
+                listOfProblems.Add(ProblemParts.CPU);
             }
             //check cpu trotling
             if(data.cpu.temp > 70.0 && data.cpu.load < 50.0)
             {
-                listOfProblems.Add(Problem.CPU);
+                listOfProblems.Add(ProblemParts.CPU);
             }
 
             //check gpu temp
             if (data.gpu.temp > 90.0)
             {
-                listOfProblems.Add(Problem.GPU);
+                listOfProblems.Add(ProblemParts.GPU);
             }
             //check gpu load
             if (data.gpu.load > 90.0)
             {
-                listOfProblems.Add(Problem.GPU);
+                listOfProblems.Add(ProblemParts.GPU);
             }
             //check gpu memload
             if (data.gpu.gpumem_load > 90.0)
             {
-                listOfProblems.Add(Problem.GPU);
+                listOfProblems.Add(ProblemParts.GPU);
             }
             //check gpu trotling
             if (data.gpu.temp > 90.0 && data.gpu.load < 50.0)
             {
-                listOfProblems.Add(Problem.GPU);
+                listOfProblems.Add(ProblemParts.GPU);
+            }
+
+            if (listOfProblems.Count == 0)
+                listOfProblems.Add(ProblemParts.OK);
+
+            return listOfProblems;
+        }
+
+        private List<Problems> analyzeProblems(DataClass data)
+        {
+            var listOfProblems = new List<Problems>();
+            //Check load hdd
+            foreach (var hdd in data.hdd)
+            {
+                if (hdd.used_space > 90.0)
+                {
+                    listOfProblems.Add(Problems.HDD_LACK_OF_MEMORY);
+                }
+            }
+            //check load ram
+            if (data.memory.used_space > 95.0)
+            {
+                listOfProblems.Add(Problems.RAM_LACK_OF_MEMORY);
+            }
+            //check cpu temp
+            if (data.cpu.temp > 70.0)
+            {
+                listOfProblems.Add(Problems.CPU_OVERHEAT);
+            }
+            //check cpu load
+            if (data.cpu.load > 90.0)
+            {
+                listOfProblems.Add(Problems.CPU_OVERLOAD);
+            }
+            //check cpu trotling
+            if (data.cpu.temp > 70.0 && data.cpu.load < 50.0)
+            {
+                listOfProblems.Add(Problems.CPU_TROTTING);
+            }
+
+            //check gpu temp
+            if (data.gpu.temp > 90.0)
+            {
+                listOfProblems.Add(Problems.GPU_OVERHEAT);
+            }
+            //check gpu load
+            if (data.gpu.load > 90.0)
+            {
+                listOfProblems.Add(Problems.GPU_OVERLOAD);
+            }
+            //check gpu memload
+            if (data.gpu.gpumem_load > 90.0)
+            {
+                listOfProblems.Add(Problems.GPU_LACK_OF_MEMORY);
+            }
+            //check gpu trotling
+            if (data.gpu.temp > 90.0 && data.gpu.load < 50.0)
+            {
+                listOfProblems.Add(Problems.GPU_TROTTING);
             }
             return listOfProblems;
         }
